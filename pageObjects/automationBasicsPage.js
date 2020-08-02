6ad5de8ee9ec08b7826ff6767dbdb953a1c4aaca
Quincy Milligan <quincymilligan@Quincys-Air.home>
2020-08-02 16:49:53 -0400

    automationBasicsPage.js

---
+++ pageObjects/automationBasicsPage.js
@@ -0,0 +1,28 @@
+module.exports = {
+    url: 'https://devmountain-qa.github.io/Automation-Basics/build/',
+    elements: {
+        evenOddInput: 'input[name="evenOddInput"]',
+        evenOddButton: 'button[name="evenOddButton"]',
+        evenResults: 'span[name="evenResults"]',
+        oddResults: 'span[name="oddResults"]',
+        evenOddBox: 'div[class="puzzleBox evenAndOddPB"]',
+
+        objectFilterInput: 'input[name="objectFilterInput"]',
+        objectFilterButton: 'button[name="objectFilterButton"]',
+        objectFilterResults: 'span[name="objectFilterResults"]',
+
+        stringFilterInput: 'input[id="nameFilterInput"]',
+        stringFilterButton: 'button[id="nameFilterButton"]',
+        stringFilterResults: 'span[name="nameFilterResults"]',
+
+        palindromeInput: 'input[name="palindromeInput"]',
+        palindromeButton: 'button[name="palindromeButton"]',
+        palindromeResults: 'span[name="palindromeResults"]',
+
+        sumInput1: 'input[name="sumInput1"]',
+        sumInput2: 'input[name="sumInput2"]',
+        sumButton: 'button[name="sumButton"]',
+        sumResults: 'span[name="sumResults"]'
+
+    }
+}
